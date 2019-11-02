2d5be2f940ee111e07544b68f13493c054dd714b
divyam02 <divyamanshumaan2017@gmail.com>
2019-11-02 13:38:32 +0530

    Added train loop

--- ML-M2019/assignment-3/Q1.py
+++ ML-M2019/assignment-3/Q1.py
@@ -5,7 +5,7 @@ import numpy as np
 #####################################################################
 
 class Neural_Net():
-	def __init__(self, layers=2, nodes_per_layer, activation_fn='relu', lr=0.001, gamma=0.5):
+	def __init__(self, layers=2, nodes_per_layer=[], activation_fn='relu', lr=0.001, gamma=0.5):
 		"""
 		Returns neural net object
 
@@ -18,11 +18,11 @@ class Neural_Net():
 
 		self.weights = dict()
 		self.bias = dict()
-		nodes_per_hidden_layer = nodes_per_layer[1:]
+		# nodes_per_hidden_layer = nodes_per_layer[1:]
 
 		for i in range(layers-1):
-			self.weights[i] = 0.01 * np.random.normal(0, 1, nodes_per_hidden_layer[i])
-			self.bias[i] = np.zeros(nodes_per_hidden_layer[i])
+			self.weights[i] = 0.01 * np.random.normal(0, 1, (nodes_per_layer[i+1], nodes_per_layer[i]))
+			self.bias[i] = np.zeros(nodes_per_layer[i+1])
 
 
 	def fit(self, train_data, train_labels, batch_size, epochs):
@@ -31,22 +31,54 @@ class Neural_Net():
 		Assume all preprocessing has been done.
 		"""
 		raise NotImplementedError
+
 		self.batch_size = batch_size
+		sample_count, feat_count = list(train_data.shape)
+		train_len = sample_count//self.batch_size
+		prev_epoch_loss = float('inf')
+		epoch_error = np.zeros(shape=(epochs,))
 
 		for epoch in range(epochs):
+			epoch_loss = 0
+			shuffled_permutation = np.random.permutation(sample_count)
+			shuffled_data = np.copy(train_data[shuffled_permutation])
+			shuffled_labels = np.copy(train_labels[shuffled_permutation])
 
+			for batch in train_len:
+				train_batch = shuffled_data[batch*self.batch_size:(batch+1)*self.batch_size]
+				label_batch = shuffled_labels[batch*self.batch_size:(batch+1)*self.batch_size]
+
+				fc_output = self.forward(train_batch)
+				class_probs = self.softmax(fc_output)
+
+				loss = self.get_CE_loss(class_probs, label_batch)
+				self.backward(loss)
+				epoch_loss += loss
+
+				print("Batch Iter: %4d/%4d, \tEpoch: %2d/%2d, \tLoss: %.4f, \tlr: %.2e\n" % (batch+1, train_len, epoch+1, epochs, loss, self.lr))
+
+			epoch_error[epoch] = epoch_loss
+			if has_loss_converged(prev_epoch_loss, epoch_loss):
+				self.lr_decay()
+
+		print("Training complete. Saving weights...")
+		np.save(activation_fn+'_MNIST', np.asarray([self.weights, self.bias]))
 
 	def predict(self, test_data):
 		"""
 		Return class probabilites for test data.
+		Expects preprocessed data!
 		"""
-		raise NotImplementedError
+		# raise NotImplementedError
+		return self.softmax(self.forward(test_data))
 
 	def score(self, test_data, test_labels):
 		"""
 		Return prediction accuracy.
+		Expects preprocessed data
 		"""		
-		raise NotImplementedError
+		# raise NotImplementedError
+		return np.sum(test_labels==np.argmax(np.predict(test_data), axis=0))/len(test_labels)
 
 	def relu(self, x):
 		"""
@@ -80,12 +112,13 @@ class Neural_Net():
 		# raise NotImplementedError
 		return (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
 
-	def softmax(self, x, label_index):
+	def softmax(self, x):
 		"""
 		Return Softmax activation
 		"""
 		# raise NotImplementedError
-		return np.exp(x[label_index])/(np.sum(np.exp(x)))
+		total = (np.sum(np.exp(x)))
+		return np.exp(x)/total
 
 	def d_relu(self, x):
 		"""
@@ -128,7 +161,7 @@ class Neural_Net():
 		if label_index==var_index: k_delta = 1
 		return self.softmax(x, label_index)*(k_delta - self.softmax(x, var_index))
 
-	def lr_decay(self, current_lr):
+	def lr_decay(self):
 		"""
 		Decay current learning rate.
 		"""
@@ -145,11 +178,23 @@ class Neural_Net():
 	def get_CE_loss(self, soft_fc_output, truth_dist, batch_size=None):
 		"""
 		Return cross entropy loss. Assuming batch to be 
-		something like mxn shape, 
+		something like mxn shape.
 		"""
 		# raise NotImplementedError
 		temp = np.sum(np.multiply(truth_dist, np.log(soft_fc_output+1e-12)))
 		return -1 * temp / self.batch_size
+
+	def forward(self, batch):
+		"""
+		Return batch logits output of network
+		"""
+		raise NotImplementedError
+
+	def backward(self):
+		"""
+		Update weights and biases!
+		"""
+		raise NotImplementedError
 
 #####################################################################
 #						  Auxillary Methods							#
